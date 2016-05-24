0272a5f926e1454cde6027bc49607e058fb1bf80
Stanislav Gernyak <stager94@mail.ru>
2016-05-24 17:12:53 +0300

    -

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,6 @@
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
 //= link_tree ../images
+//= link_tree ../images/demo
+//= link_tree ../images/ui
+//= link_tree ../images/ui/icons
