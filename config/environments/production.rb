ad2be53518fdfc98b298458e772a8ef856246c2e
Stanislav Gernyak <stager94@mail.ru>
2016-07-04 00:07:19 +0300

    -

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,14 +80,11 @@ Rails.application.configure do
 
 
   config.action_mailer.delivery_method = :sendmail
-  # Defaults to:
-  # config.action_mailer.sendmail_settings = {
-  #   location: '/usr/sbin/sendmail',
-  #   arguments: '-i -t'
-  # }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'no-reply@example.com'}
+  config.action_mailer.default_url_options = { :host => "146.185.148.161" }
+
 
   config.action_dispatch.rescue_responses.merge!(
     'AuthorizationException' => :unauthorized
