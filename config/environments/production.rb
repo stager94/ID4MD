1dab1d0756de7140df14a4acc1cbbd263545cc1c
Stanislav Gernyak <stager94@mail.ru>
2016-07-04 00:04:00 +0300

    -

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,14 +79,17 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => '146.185.148.161',
-  :user_name            => 'stager94@gmail.com',
-  :password             => 'rooney94',
-  :authentication       => 'plain',
-  :enable_starttls_auto => true  }
-  config.action_mailer.default_url_options = { host: '146.185.148.161' }
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i -t'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@example.com'}
+
+  config.action_dispatch.rescue_responses.merge!(
+    'AuthorizationException' => :unauthorized
+  )
 end
