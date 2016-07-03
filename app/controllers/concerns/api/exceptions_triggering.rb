3728c3813e9ee63f195a53a0096eeaebeec8ce01
Stanislav Gernyak <stager94@mail.ru>
2016-07-03 22:46:05 +0300

    setting up mailer

--- app/controllers/concerns/api/exceptions_triggering.rb
+++ app/controllers/concerns/api/exceptions_triggering.rb
@@ -5,7 +5,7 @@ module API::ExceptionsTriggering
 
 	included do
 		rescue_from ::StandardError do |exception|
-			render json_failed(exception.message, exception.try(:code) || 500, { name: exception.class.name })
+			render json_failed(exception.message, exception.try(:code) || 500, { name: exception.class.name, errors: [exception.message] })
 		end
 	end
 	
